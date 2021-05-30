5da5ce662fbc44707d7cbc2250064d9f5c21cf3a
ssilen <Witcher696>
2021-05-30 23:49:17 +0300

    Homework

--- Lecture_4.2/script.py
+++ Lecture_4.2/script.py
@@ -10,14 +10,18 @@ parser.add_argument("--dir")
 args = parser.parse_args()
 dir = args.dir
 if dir == None:
-    dir = "cd ~/netology/sysadm-homeworks"
+    dir = "~/netology/sysadm-homeworks"
 
 dir = "cd " + dir
-print(dir)
-bash_command = ["cd ~/git/homework", "git status", "pwd"]
+temp = dir + ' && ' + 'ls -a | grep .git | wc -l'
+test_dir = int(os.popen(temp).read())
+if test_dir == 0:
+    print(' This directory hasnt git repostitory' )
+    quit()
+bash_command = [dir, "git status", "pwd"]
 result_os = os.popen(' && '.join(bash_command)).read()
 is_change = False
-list_changed = ('modified', 'new file', 'изменено', 'новый файл')
+list_changed = ('modified', 'изменено')
 full_dir = result_os.split('\n')[-2]
 for result in result_os.split('\n'):
     for now_change in list_changed:
@@ -27,4 +31,4 @@ for result in result_os.split('\n'):
             prepare_result = full_dir + '/' + prepare_result
             prepare_result = prepare_result.replace('  ', '')
             print(prepare_result)
-            break
+            #break
